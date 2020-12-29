366908b3d2e79bdb014d80b76d6f5d1b8199a872
Joachim Karlsson <joachim.karlsson@utb.ecutbildning.se>
2020-12-29 18:13:02 +0100

    form

---
+++ inlamningsuppgift1-form/js/script.js
@@ -0,0 +1,17 @@
+const form = document.getElementById('form');
+const firstName = document.getElementById('firstname');
+const lastName = document.getElementById('lastname');
+const email = document.getElementById('email');
+const identification = document.getElementById('identification');
+
+
+form.addEventListener ('submit', (e) => {
+    e.preventDefault();
+
+    checkInputs();
+});
+
+
+checkInputs() => {
+    
+}
