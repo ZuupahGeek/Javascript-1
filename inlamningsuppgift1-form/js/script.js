daca8ecc0a6ecc209f1dbfd1e5cc79bc79d22c48
Joachim Karlsson <joachim.karlsson@utb.ecutbildning.se>
2021-01-28 22:42:47 +0100

    försök till edit-funktion

--- inlamningsuppgift1-form/js/script.js
+++ inlamningsuppgift1-form/js/script.js
@@ -5,7 +5,7 @@ const email = document.querySelector('#email');
 const idNumber = document.querySelector('#id');
 const output = document.querySelector('#users')
 const outputButton = document.querySelector('#user-buttons')
-const btnRemove = document.querySelector(".btn-remove")
+
 
 idNumber.value = create_UUID();
 let users = [];
@@ -59,28 +59,6 @@ function setSuccessFor(input) {
     formControl.className = 'form-control success';
 }
 
-const renderUser = () => {
-
-    output.innerHTML = '',
-    users.forEach(user => {
-
-        let html = `
-        <div class="container users">
-                <div class="user-text">
-                    <h4>${user.firstName} ${user.lastName}</h4>
-                    <p>${user.email}</p>
-                </div>
-                <div id="${user.idNumber}" class="user-buttons">
-                    <button class="btn btn-remove">Remove</button>
-                    <div class="edit-button">
-                        <button class="btn btn-edit">Edit</button>  
-                    </div> 
-            </div>
-        `
-        output.innerHTML += html
-    })
-
-}
 
 const createUser = (firstName, lastName, email,) => {
     let user = {
@@ -88,11 +66,126 @@ const createUser = (firstName, lastName, email,) => {
         firstName,
         lastName,
         email
-    }
+    };
     
     users.push(user);
-    console.log(users)
+    console.log(users);
 }
+
+
+const renderUser = () => {
+
+    
+    output.innerHTML = '',
+    users.forEach(user => {
+        
+        let userContainer = document.createElement('div');
+        userContainer.classList.add('container', 'users');
+
+        let userText = document.createElement('div');
+        userText.classList.add('user-text');
+
+        let fullName = document.createElement('h4');
+        fullName.innerText = `${user.firstName} ${user.lastName}`;
+
+        let emailText = document.createElement('p');
+        emailText.innerText = user.email;
+
+        let userButtons = document.createElement('div');
+        userButtons.classList.add('user-buttons');
+        userButtons.setAttribute('id', `${user.idNumber}`);
+
+        let btnRemove = document.createElement('button');
+        btnRemove.classList.add('btn', 'btn-remove');
+        btnRemove.innerText = 'Remove';
+        btnRemove.addEventListener('click', e => {
+            console.log('ta bort, lol');
+            users = users.filter(user => (user.idNumber) !== e.target.parentNode.id);
+            renderUser();
+        });
+
+        let btnEdit = document.createElement('button');
+        btnEdit.classList.add('btn', 'btn-edit');
+        btnEdit.innerText = 'Edit';
+        btnEdit.addEventListener('click', e => {
+            console.log('ändra användare, lol');
+            form.removeEventListener('submit', addUser);
+            userIndex = users.findIndex((user => user.idNumber === e.target.parentNode.id))
+        
+        editUser();
+        
+        });
+
+        userButtons.appendChild(btnEdit);
+        userButtons.appendChild(btnRemove);
+        userText.appendChild(fullName);
+        userText.appendChild(emailText);
+        userContainer.appendChild(userText);
+        userContainer.appendChild(userButtons);
+        output.appendChild(userContainer);
+
+
+       /*  let html = `
+        <div class="container users">
+                <div class="user-text">
+                    <h4>${user.firstName} ${user.lastName}</h4>
+                    <p>${user.email}</p>
+                </div>
+                <div id="${user.idNumber}" class="user-buttons">
+                    <button class="btn btn-remove">Remove</button>
+                        <button class="btn btn-edit">Edit</button>
+            </div>
+        `
+       output.innerHTML += html;
+        let btnRemove = document.querySelector(".btn-remove");
+        let btnEdit = document.querySelector(".btn-edit")
+   
+
+   
+
+        btnRemove.addEventListener('click', e => {
+            console.log('ta bort');
+            users = users.filter(user => (user.idNumber) !== e.target.parentNode.id);
+            renderUser();
+        });
+
+        btnEdit.addEventListener('click', e => {
+            console.log('ändra');
+            
+        userIndex = users.findIndex((user => user.idNumber == e.target.parentNode.id))
+        
+        editUser();
+        renderUser();
+        })
+         */
+    });
+}
+
+const editUser = () => {
+    console.log(users[userIndex])
+    firstName.value =  users[userIndex].firstName
+    lastName.value =  users[userIndex].lastName
+    email.value =  users[userIndex].email
+    idNumber.value = users[userIndex.idNumber]
+
+   
+
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+        console.log('ny submit funkar')
+        users[userIndex].firstName = firstName.value;
+        users[userIndex].email = email.value;
+        users[userIndex].lastName = lastName.value;
+
+        console.log(users[userIndex])
+
+       form.reset();
+        form.addEventListener('submit', addUser);
+    })
+    renderUser();
+    
+}
+
 
 
 // Genererar ID-nummer
@@ -110,7 +203,7 @@ function create_UUID(){
 
 
  
-form.addEventListener ('submit', (e) => {
+/* form.addEventListener ('submit', (e) => {
     e.preventDefault();
 
     checkInputs();
@@ -120,27 +213,20 @@ form.addEventListener ('submit', (e) => {
         renderUser();
         form.reset();
     }
-});
+}); */
 
-output.addEventListener ('click', (e) => {
-    e.preventDefault();
-    // console.log("hejhej!")
-    users = users.filter(user => (user.idNumber) !== e.target.parentNode.id)
-    renderUser();
-    console.log(users)
-   
-});
+var addUser = function (event) {
+    event.preventDefault();
+    checkInputs();
 
-output.addEventListener ('click', (e) => {
-    e.preventDefault();
-   
-    users = users.find(user => (user.idNumber) !== e.target.parentNode.parentNode.id)
-    return user.idNumber
-    console.log(user.idNumber)
-    
-    
-   
-});
+    if(firstName.value !== '' && lastName.value !== '' && email.value !== '' && validateEmail(email.value)) {
+        createUser(firstName.value, lastName.value, email.value);
+        renderUser();
+        form.reset();
+    }
+}
+
+form.addEventListener('submit', addUser);
 
 
 
